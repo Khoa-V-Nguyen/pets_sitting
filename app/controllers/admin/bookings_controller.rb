b52b7473f862a4ad781366c68308494c44144f20
Khoa Nguyen <khoa.nguyen@hotelengine.com>
2023-05-21 14:58:16 -0600

    Created `admin/edit` and action
    Added `Delete booking` in `edit` page
    Added `admin/bookings_controller#destroy`
    Added `admin/bookings_controller#update`
    Added `admin/bookings_controller#edit
    Added `_form_fields` partial
    Updated `bookings/new``

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -2,4 +2,39 @@ class Admin::BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update(booking_params)
+      flash[:notice] = 'Booking was successfully updated.'
+      redirect_to admin_bookings_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Booking.find(params[:id]).destroy
+
+    flash[:notice] = 'Booking was successfully deleted.'
+    redirect_to admin_bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :first_name,
+      :last_name,
+      :animal_name,
+      :animal_type,
+      :hours_requested,
+      :date_of_service
+    )
+  end
 end
